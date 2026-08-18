942f1bf45b69b3f9e9e5726eb6782da33d80e3f7
lrcordell <lee.cordell88@gmail.com>
2026-08-18 16:13:50 -0500

    Update fetch-prices.js

--- api/fetch-prices.js
+++ api/fetch-prices.js
@@ -13,7 +13,7 @@ function getRedisClient() {
 }
 
 const CACHE_KEY = 'live-prices-cache';
-const CACHE_TTL_SECONDS = 600;
+const CACHE_TTL_SECONDS = 1200;
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
